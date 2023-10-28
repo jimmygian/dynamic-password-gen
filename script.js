753c73b1b437f309e59f6309de3929e9cc73bc11
Dimitris Giannoulis <dimitrisgiannoulis@Dimitriss-Mini.cust.communityfibre.co.uk>
2023-10-28 15:31:12 +0100

    Correctly shuffles array

--- script.js
+++ script.js
@@ -91,35 +91,34 @@ var hasUpperCasedCharacters = [
 // ==================================== //
 
 
-// Function to prompt user for password options
+// A) Function to prompt user for password options
 function getPasswordOptions() {
   
-  // 1. Get length of p/w user prefers, between 8 - 128 inclusive
+  // 1. Gets length of p/w user prefers, between 8 - 128 inclusive
   var pwLength = prompt("Choose password length (between 8 - 128 chars):");
 
-  // Evaluate user input:
+  // Evaluates user input:
   while (pwLength !== null) {
-    // Try to make user input a number
+    // Tries to make user input a number
     pwLength = parseInt(pwLength);
     
-    // Evaluate user input
     if (pwLength >= 8 && pwLength <= 128 && !isNaN(pwLength)) {
-      // Break if user input is correct
+      // Breaks if user input is correct
       break;
     } else {
-      // Re-prompt if user input is incorrect
+      // Re-prompts if user input is incorrect
       pwLength = prompt("Please insert a numberic value between 8 -128. Choose password length:");
     }
   }
-  // Test pwLength output in console
+  // Tests pwLength output in console
   console.log("Password length is:", pwLength);
 
-  // Return if user canceled
+  // Returns if user canceled
   if (pwLength === null) {
     return null;
   }
 
-  // 2. Get prefered types (at least 1 of those char classes should be selected)
+  // 2. Gets prefered types (at least 1 of those char classes should be selected)
 
     // lowercase
     var hasLowercase = confirm("Should password include lowercase?");
@@ -135,7 +134,7 @@ function getPasswordOptions() {
     console.log("hasSpecial:", hasSpecial);
   
 
-  // 3. Return an object with all user's p/w preferences
+  // 3. Returns an object with all user's p/w preferences
   var pwOptions = {
     length: pwLength,
     lowercase: hasLowercase,
@@ -144,7 +143,7 @@ function getPasswordOptions() {
     special: hasSpecial
   };
 
-  // Check if at least 1 char class was chosen, return null if none was.
+  // Checks if at least 1 char class was chosen, return null if none was.
   if (pwOptions.lowercase === false && pwOptions.uppercase === false && pwOptions.numeric === false && pwOptions.special === false) {
     alert("ERROR: At least 1 of the available character classes must be chosen. Please try again.")
     return null;
@@ -154,28 +153,44 @@ function getPasswordOptions() {
 
 
 
-
-// Function for getting a random element from an array
+// B) Function for getting a random element from an array
 function getRandom(arr) {
 
-  return "I will be returning an array of randomized chars."
+  // Creates a variable to store the randomized array
+  var randomArr = [];
+  
+  // Copies array so that original is not modified
+  var unshuffledArr = [];
+  var unshuffledArr = unshuffledArr.concat(arr);
+
+  // Shuffles array
+  while (unshuffledArr.length !== 0) {
+    // Chooses a random number between 0 and array's length
+    var j = Math.floor(Math.random() * (unshuffledArr.length));
+    // Concats the char of that index to randomArr
+    randomArr = randomArr.concat(unshuffledArr[j])
+    // Removes that char from the unshuffled array
+    unshuffledArr.splice(j, 1);
+  }
+  
+  // Returns randomized array
+  return randomArr;
 }
 
 
 
-
-// Function to generate password with user input
+// C) Function to generate password with user input
 function generatePassword() {
   
-  // Get P/W options, return if null
+  // Gets P/W options, return if null
   var pwOptions = getPasswordOptions()
-  console.log("'pwOptions' object:", pwOptions)
+  // console.log("'pwOptions' object:", pwOptions)
 
   if (pwOptions === null) {
     return "** Try again! **"
   }
   
-  // Generate arr of acceptable chars
+  // Generates arr of acceptable chars
   var allowedChars = [];
   if (pwOptions.lowercase) {
     allowedChars = allowedChars.concat(hasLowerCasedCharacters)
@@ -190,13 +205,13 @@ function generatePassword() {
     allowedChars = allowedChars.concat(hasSpecialCharacters)
   }
 
-  // Log allowed chars in console
+  // Logs allowed chars in console
   console.log("allowedChars:", allowedChars)
 
-  
-  // Get randomized array
+  // Gets randomized array
   var randomCharArr = getRandom(allowedChars);
   console.log("Random Char Array:", randomCharArr)
+  // console.log("allowedChars:", allowedChars)
 
 
   return "WORKS!!"
